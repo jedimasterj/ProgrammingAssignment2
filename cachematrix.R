06ff28d1f49ec98ed38ca45124ac6874a4af6d98
oj===js <ojsingh@gmail.com>
2015-11-22 18:19:36 -0500

    Updates
    Updates for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,11 @@
-## Put comments here that give an overall description of what your
+getw## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This is whre the new function will be written
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  ## 
 
 }
 
@@ -12,4 +14,6 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## This is where the new functin will be written
+        ## take the parameters in, set defaults, and write the function.
 }
