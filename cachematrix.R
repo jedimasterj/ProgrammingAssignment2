edc77508be8316daf102fc4b1ea78f7b93ef607a
oj===js <ojsingh@gmail.com>
2015-11-22 19:23:31 -0500

    Final submission - major updates
    Code has been fleshed out and comments added. This is my submission for
    this assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,65 @@
-getw## Put comments here that give an overall description of what your
-## functions do
 
-## This is whre the new function will be written
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## This is broken down into a list of the following 4 functions:
+
+## 1. set the value of the matrix  - this is used when we want to update the vector to a different environment.
+## 2. get the value of the matrix - return the value of the vector (that has already been set)
+## 3. invert and set the (inverted) matrix  - store the calculated value in the main function 'makeCacheMatrix'
+## 4. get the (inverted) matrix - return the calculated value in the main function 'makeCacheMatrix'
+
+## In the context of this assignment, these functions will be called upon when needed by the 'cacheSolve' function
+## to avoid duplicating previous calculations to invert a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-  
   m<-NULL
+  
+  ## function 1 - set the value of the matrix (note the '<<' operator meaning it is returned to a different environment)
   set<-function(y){
       x<<-y
-      m<<-NULL    ## set defaults
-  }
+      m<<-NULL }
 
+  ## function 2 - get the value of the matrix
   get<-function() x
-  setmatrix<-function(solve) m<<- solve()## Call the solve function
+  
+  ## function 3 - invert and set the value of the matrix
+  invertmatrix<-function(solve) m<<- solve() 
+  
+  ## function 4 - get the inverted value of the matrix 
   getmatrix<-function() m
-  list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
+  
+  ##store the 4 created functions in a list
+  list(set=set, get=get, invertmatrix=invertmatrix, getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
+
+
+
+## Function 'cacheSolve'cacheSolve: This function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix 
+## has not changed), then the cachesolve should retrieve the inverse from the cache.
+
+## The logic followed in this function is basically a single IF () ELSE () statement.
+## Verify the value m (wich was stored previously) exists and is not NULL. 
+## If it exists, return a message and the value m. The context in this assignment, is that it would 
+## skip the computation needed to invert the matrix, thereby saving time and being more efficient.
+## Otherwise (i.e. If it doesn't exist or it is null) then solve the matrix and store it.
 
 cacheSolve <- function(x, ...) { ## placeholder for parametes
-        ## Return a matrix that is the inverse of 'x'
-        ## This is where the new functin will be written
-        ## take the parameters in, set defaults, and write the function.
-  
+
+    ## If m exists, and is not NULL return a message and the value m.
     m<-x$getmatrix()
     if(!is.null(m)){
-        message("getting data ")
+        message("retrieving existing matrix")
         return(m)
     }
-    matrix<-x$get()
-    m<-solve(matrix, ...)
-    x$setmatrix(m)
-    m
+    
+    ## Otherwise, get the matrix, invert it, and then set the inverted matrix (in essence 'caching' it)
+    else{
+        matrix<-x$get()
+        m<-solve(matrix, ...)
+        x$invertmatrix(m)
+        m  
+    }
+    
 }
